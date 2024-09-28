f35fa87e51f3e6c7c3b969d7356f63f0ca48fac9
IlhanCodeAc <ilhanma@code.edu.az>
2024-09-28 23:23:08 +0400

    bugs fixed

--- components/ui/deleteButton.tsx
+++ components/ui/deleteButton.tsx
@@ -1,19 +1,19 @@
-"use client";
-import React from 'react';
-import { Button } from './button';
-import { deleteProduct } from '@/app/(actions)/product';
+// "use client";
+// import React from 'react';
+// import { Button } from './button';
+// import { deleteProduct } from '@/app/(actions)/product';
 
-interface DeleteButtonProps {
-    id: string;
-}
+// interface DeleteButtonProps {
+//     id: string;
+// }
 
-const DeleteButton: React.FC<DeleteButtonProps> = (props) => {
-    console.log(props)
-    return (
-        <Button onClick={() => deleteProduct(props.id)}>
-            Delete
-        </Button>
-    );
-};
+// const DeleteButton: React.FC<DeleteButtonProps> = (props) => {
+//     console.log(props)
+//     return (
+//         <Button onClick={() => deleteProduct(props.id)}>
+//             Delete
+//         </Button>
+//     );
+// };
 
-export default DeleteButton;
+// export default DeleteButton;
