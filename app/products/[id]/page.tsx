cd74a37417653cca8d1a738bb66c5a914435de78
IlhanCodeAc <ilhanma@code.edu.az>
2024-10-01 11:10:55 +0400

    product detail design fixed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -3,6 +3,7 @@ import GetByID from "@/app/actions/products";
 import { Container } from "@mui/material";
 import style from "./style.module.scss";
 import DeleteButton from "@/app/_components/Delete/Delete";
+import Addtocart from "@/app/_components/AddToCartBTN/Addtocart";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   let product;
@@ -34,25 +35,27 @@ const Page = async ({ params }: { params: { id: string } }) => {
             allowFullScreen
           />
         </div>
-        <div className={style.ProductRight}>
-          <div className={style.ProductDesc}>
-            <h3>{product.description}</h3>
-
-            <div className={style.ProductImages}>
+        <div className={style.ProductImages}>
               <img src={product.sliderImageOne} alt="" />
               <img src={product.sliderImageTwo} alt="" />
               <img src={product.sliderImageThree} alt="" />
             </div>
-          </div>
-        </div>
-        <div className={style.ProductPurchase}>
+        
+        <div className={style.ProductRight}>
+          <div className={style.ProductDesc}>
+            <h3>{product.description}</h3>
+            <div className={style.ProductPurchase}>
           <h1>Purchase Info:</h1>
           <div className={style.PurchaseInfo}>
             <DeleteButton id={product.id} />
             <h2 className={style.ProductPrice}>{product.price}$</h2>
-            <button className={style.SendToCart}>Add To Cart</button>
+            <Addtocart productId={product.id}/>
           </div>
         </div>
+        
+          </div>
+        </div>
+       
       </Container>
     </>
   );
