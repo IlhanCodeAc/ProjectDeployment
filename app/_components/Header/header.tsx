74f3b72bd9e174290b990ae181433921ed150e85
IlhanCodeAc <ilhanma@code.edu.az>
2024-10-04 19:35:41 +0400

    last corrections before deployment

--- app/_components/Header/header.tsx
+++ app/_components/Header/header.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 import style from './styles.module.scss';
 import { Container } from '@mui/material';
 import Image from 'next/image';
 import phoenixLogo from './372-3728490_red-and-black-b-logo-gaming-logos-red.png';
 import Link from 'next/link';
-import { UserButton} from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
 import { ROLE } from '@prisma/client';
 import { RenderIf } from '../RenderIf/RenderIf';
 import prisma from '@/app/lib/db';
@@ -13,7 +12,7 @@ import { auth } from '@clerk/nextjs/server';
 
 interface HeaderProps {
   user?: {
-    role: string; 
+    role: string;
   };
 }
 
@@ -22,7 +21,31 @@ const Header: React.FC<HeaderProps> = async ({ }) => {
   const { userId } = auth();
 
   if (!userId) {
-    throw new Error("User not authenticated");
+    return (
+      <div className={style.NavContainer}>
+        <Container>
+          <div className={style.nav}>
+            <Link href={'/'} className={style.NavLeft}>
+              <Image
+                src={phoenixLogo}
+                alt="Phoenix Logo"
+                width={50}
+                height={50}
+              />
+              <h3 className={style.LogoName}>Fiery Nest</h3>
+            </Link>
+            <div className={style.Right}>
+              <Link href="/products">Games</Link>
+              <Link href="/Cart">Cart</Link>
+              <Link href={"/sign-up"}>Sign-up</Link>
+              <Link href={"/sign-in"}>Sign-in</Link>
+
+              <UserButton />
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
   }
 
   const user = await prisma.user.findUnique({
@@ -30,9 +53,10 @@ const Header: React.FC<HeaderProps> = async ({ }) => {
       externalId: userId,
     },
   });
+
   const isAdmin = user?.role === ROLE.Admin;
+
   return (
-    <>
     <div className={style.NavContainer}>
       <Container>
         <div className={style.nav}>
@@ -48,22 +72,15 @@ const Header: React.FC<HeaderProps> = async ({ }) => {
           <div className={style.Right}>
             <Link href="/products">Games</Link>
             <Link href="/Cart">Cart</Link>
-            <RenderIf condition ={isAdmin}>
-            <Link href="/Creation">Add new game</Link>
+            <RenderIf condition={isAdmin}>
+              <Link href="/Creation">Add new game</Link>
             </RenderIf>
-            {/* {!isSignedIn && (
-              <>
-                <Link href="/sign-up">Sign Up</Link>
-                <Link href="/sign-in">Sign In</Link>
-              </>
-            )} */}
             <UserButton />
           </div>
         </div>
       </Container>
     </div>
-    </>
   );
-}
+};
 
 export default Header;
